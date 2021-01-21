48b6bdd5f03302693c424be55eb5505cfa10393e
Seishin <ykohler@gmail.com>
2021-01-21 10:44:45 +0100

    Update Editer_parcours_navbar.user.js

--- formateurs/Editer_parcours_navbar.user.js
+++ formateurs/Editer_parcours_navbar.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Editer_parcours_navbar
 // @namespace    metis.afpa.fr
-// @version      0.1
+// @version      0.2
 // @description  display Editer button in the navbar (Formateurs only)
 // @author       yannick.kohler@afpa.fr
 // @match        https://metis.afpa.fr/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @downloadURL https://github.com/seishin77/metis-hacks/raw/master/formateurs/Editer_parcours_navbar.user.js
+// @updateURL   https://github.com/seishin77/metis-hacks/raw/master/formateurs/Editer_parcours_navbar.meta.js
 // ==/UserScript==
 
 /* global jQuery, $ */

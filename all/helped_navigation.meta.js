aff446d2f626185e9282a6881834ecba76722436
Seishin <ykohler@gmail.com>
2021-01-21 10:51:05 +0100

    Create helped_navigation.meta.js

---
+++ all/helped_navigation.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         helped_navigation
+// @namespace    metis.afpa.fr
+// @version      0.1
+// @description  display the section number in the left navbar
+// @author       yannick.kohler@afpa.fr
+// @match        https://metis.afpa.fr/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @downloadURL https://github.com/seishin77/metis-hacks/raw/master/formateurs/helped_navigation.user.js
+// @updateURL   https://github.com/seishin77/metis-hacks/raw/master/formateurs/helped_navigation.meta.js
+// ==/UserScript==
